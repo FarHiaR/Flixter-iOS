7c02c6e960333e49f0c3bc86d1eb25a6577e3ecd
Fariha <fariha.rashid99@gmail.com>
2021-09-11 22:28:08 -0400

    Initial Commit

---
+++ Flixster(iOS)/MovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCell.swift
+//  Flixster(iOS)
+//
+//  Created by Fariha Rashid on 9/10/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ Pods/Target Support Files/Pods-Flixster(iOS)-Flixster(iOS)UITests/Pods-Flixster(iOS)-Flixster(iOS)UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flixster_iOS__Flixster_iOS_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Flixster_iOS__Flixster_iOS_UITests
+@end
